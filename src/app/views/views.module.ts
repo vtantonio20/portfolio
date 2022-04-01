17549998c6048be15f4403f7a62cbdefde65dd9f
Victor Torres <vt.antonio20@gmail.com>
2022-04-01 13:59:25 -0500

    added navbar

---
+++ src/app/views/views.module.ts
@@ -0,0 +1,20 @@
+import { CoreModule } from './../core/core.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    CommonModule,
+    CoreModule
+  ],
+  exports: [HomeComponent]
+})
+export class ViewsModule { }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Oswald']
+      },
+    },
+  },
+  plugins: [],
+}
