11b6f27a04dbd2621bbb99756112e1ec44f5da3d
Victor Torres <vt.antonio20@gmail.com>
2022-04-01 19:27:41 -0500

    home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,25 @@ module.exports = {
       fontFamily: {
         body: ['Oswald']
       },
+      animation:{
+        blob: "blob 5s infinite "
+      },
+      keyframes: {
+
+        blob: {
+          "0%": {
+            transform: " scale(1)", opacity: 1
+          },
+          "50%": {
+            transform: " scale(1.05)", opacity: 0,
+
+          },
+          "100%": {
+            transform: " scale(1)", opacity: 1,
+          }
+        }
+      },
+
     },
   },
   plugins: [],
